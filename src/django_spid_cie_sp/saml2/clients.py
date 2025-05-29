ccf271981f1bf30170d096de35d8cc599e1ae2d4
Francesco Filicetti <easyweb.wa@gmail.com>
2025-05-29 10:42:30 +0200

    fix: request Issuer definition after generalization

--- src/django_spid_cie_sp/saml2/clients.py
+++ src/django_spid_cie_sp/saml2/clients.py
@@ -8,9 +8,9 @@ from saml2.saml import Issuer
 class CIESaml2Client(Saml2Client):
     def create_authn_request(self, *args, **kwargs):
         issuer = Issuer(
-            name_qualifier=f"https://{settings.HOSTNAME}/{settings.CIE_SAML2_PREFIX}/metadata/",
+            name_qualifier=getattr(settings, "CIE_SP_ENTITY_ID", f"https://{settings.HOSTNAME}/{settings.CIE_SAML2_PREFIX}/metadata/"),
             format="urn:oasis:names:tc:SAML:2.0:nameid-format:entity",
-            text=f"https://{settings.HOSTNAME}/{settings.CIE_SAML2_PREFIX}/metadata/"
+            text=getattr(settings, "CIE_SP_ENTITY_ID", f"https://{settings.HOSTNAME}/{settings.CIE_SAML2_PREFIX}/metadata/")
         )
         req_id, req = super().create_authn_request(issuer=issuer, *args, **kwargs)
         return req_id, req
@@ -19,9 +19,9 @@ class CIESaml2Client(Saml2Client):
 class SPIDSaml2Client(Saml2Client):
     def create_authn_request(self, *args, **kwargs):
         issuer = Issuer(
-            name_qualifier=f"https://{settings.HOSTNAME}/{settings.SPID_SAML2_PREFIX}/metadata/",
+            name_qualifier=getattr(settings, "SPID_SP_ENTITY_ID", f"https://{settings.HOSTNAME}/{settings.SPID_SAML2_PREFIX}/metadata/"),
             format="urn:oasis:names:tc:SAML:2.0:nameid-format:entity",
-            text=f"https://{settings.HOSTNAME}/{settings.SPID_SAML2_PREFIX}/metadata/"
+            text=getattr(settings, "SPID_SP_ENTITY_ID", f"https://{settings.HOSTNAME}/{settings.SPID_SAML2_PREFIX}/metadata/")
         )
         req_id, req = super().create_authn_request(issuer=issuer, *args, **kwargs)
         return req_id, req
